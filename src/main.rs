d05705c8bdb09551c607ba2f03d287a1ae5807a9
dustin4242 <thegameexposer@gmail.com>
2022-02-25 11:50:30 -0600

    Started visual studio coding

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,35 @@
-use std::{process::Command, env::args, path::Path};
+use std::{env::args, path::Path, process::Command};
 fn main() -> std::io::Result<()> {
     let wallpaper: Vec<String> = args().collect();
     if wallpaper.len() >= 2 {
         if Path::new(&wallpaper[1]).exists() {
             if !wallpaper[1].ends_with(".png") != true {
-                let _walling = Command::new("wal").args([
-                    "-i",
-                    &wallpaper[1],
-                    "--saturate",
-                    "1"
-                ]).output();
-                let _set_wallpaper = Command::new("cp").args([
-                    &wallpaper[1], 
-                    "/home/dudebro/.config/awesome/themes/powerarrow-dark/wall.png"
-                ]).output();
-                let _restart_client = Command::new("awesome-client").arg("awesome.restart()").output();
+                let _walling = Command::new("wal")
+                    .args(["-i", &wallpaper[1], "--saturate", "1"])
+                    .output();
+                let _set_wallpaper = Command::new("cp")
+                    .args([
+                        &wallpaper[1],
+                        "/home/dudebro/.config/awesome/themes/powerarrow-dark/wall.png",
+                    ])
+                    .output();
+                let _restart_client = Command::new("awesome-client")
+                    .arg("awesome.restart()")
+                    .output();
+            } else {
+                error_print("Not a valid .png file")
             }
-            else { error_print("Not a valid .png file") }
+        } else {
+            error_print("Not a valid path")
         }
-        else { error_print("Not a valid path") }
+    } else {
+        match wallpaper.len() {
+            1 => println!("Usage: awesomewal [path to wallpaper]"),
+            _ => error_print("How"),
+        }
     }
-    else { match wallpaper.len() {
-        1 => error_print("Missing Wallpaper Argument"),
-        _ => error_print("How"),
-    }}
     Ok(())
 }
-fn error_print(error_label: &'static str){ println!("Error: {}", error_label) }
+fn error_print(error_label: &'static str) {
+    println!("Error: {}", error_label)
+}
